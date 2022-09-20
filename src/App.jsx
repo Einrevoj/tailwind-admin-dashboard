a20d583f94ba1c065299b495745f4aaa239e9494
Einrevoj <jovcano28@gmail.com>
2022-09-20 15:23:02 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import TopBar from "./components/TopBar";
+import Sidebar from "./components/Sidebar";
+import Home from "./pages/Home";
+import UserList from "./pages/UserList";
+import User from "./pages/User";
+import NewUser from "./pages/NewUser";
+import ProductList from "./pages/ProductList";
+import Product from "./pages/Product";
+import NewProduct from "./pages/NewProduct";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <TopBar />
+        <div className="container">
+          <Sidebar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/users" element={<UserList />} />
+            <Route path="/user/:userId" element={<User />} />
+            <Route path="/newUser" element={<NewUser />} />
+            <Route path="/products" element={<ProductList />} />
+            <Route path="/product/:productId" element={<Product />} />
+            <Route path="/newproduct" element={<NewProduct />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
