6c7383be40f7e26af581ab16dcb0482ce9959dbe
Einrevoj <jovcano28@gmail.com>
2022-09-20 17:24:43 +0800

    added top bar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -13,91 +13,171 @@ import {
   WorkOutline,
   Report,
 } from "@mui/icons-material";
+import HomeIcon from "@mui/icons-material/Home";
 import { Link } from "react-router-dom";
 
 export default function Sidebar() {
   return (
-    <div className="sidebar">
-      <div className="sidebarWrapper">
-        <div className="sidebarMenu">
-          <h3 className="sidebarTitle">Dashboard</h3>
-          <ul className="sidebarList">
-            <Link to="/" className="link">
-              <li className="sidebarListItem active">
-                <LineStyle className="sidebarIcon" />
-                Home
-              </li>
-            </Link>
-            <li className="sidebarListItem">
-              <Timeline className="sidebarIcon" />
-              Analytics
+    <div class="flex flex-row min-h-screen bg-gray-100 text-gray-500">
+      <aside class="sidebar w-64 md:shadow transform -translate-x-full md:translate-x-0 transition-transform duration-150 ease-in bg-indigo-500">
+        <div class="sidebar-header flex items-center justify-center py-4">
+          <div class="inline-flex">
+            <a href="#" class="inline-flex flex-row items-center">
+              <span class="leading-10 text-gray-100 text-2xl font-bold ml-1 uppercase">
+                VCA
+              </span>
+            </a>
+          </div>
+        </div>
+        <div class="sidebar-content px-4 py-6">
+          <ul class="flex flex-col w-full">
+            <li class="my-px">
+              <a
+                href="#"
+                class="flex flex-row items-center h-10 px-3 rounded-lg text-gray-700 bg-gray-100"
+              >
+                <span class="flex items-center justify-center text-lg text-gray-400">
+                  <HomeIcon />
+                </span>
+                <span class="ml-3">Dashboard</span>
+              </a>
             </li>
-            <li className="sidebarListItem">
-              <TrendingUp className="sidebarIcon" />
-              Sales
+            <li className="my-px">
+              <a
+                href="#"
+                class="flex flex-row items-center h-10 px-3 rounded-lg text-gray-300 hover:bg-gray-100 hover:text-gray-700"
+              >
+                <LineStyle />
+                <span class="ml-3">Home</span>
+              </a>
+            </li>
+            <li className="my-px">
+              <a
+                href="#"
+                class="flex flex-row items-center h-10 px-3 rounded-lg text-gray-300 hover:bg-gray-100 hover:text-gray-700"
+              >
+                <Timeline />
+                <span class="ml-3">Analytics</span>
+              </a>
+            </li>
+            <li className="my-px">
+              <a
+                href="#"
+                class="flex flex-row items-center h-10 px-3 rounded-lg text-gray-300 hover:bg-gray-100 hover:text-gray-700"
+              >
+                <TrendingUp />
+                <span class="ml-3">Sales</span>
+              </a>
+            </li>
+            <li class="my-px">
+              <span class="flex font-medium text-sm text-gray-300 px-4 my-4 uppercase">
+                Quick Menu
+              </span>
+            </li>
+            <li className="my-px">
+              <a
+                href="#"
+                class="flex flex-row items-center h-10 px-3 rounded-lg text-gray-300 hover:bg-gray-100 hover:text-gray-700"
+              >
+                <PermIdentity />
+                <span class="ml-3">Users</span>
+              </a>
+            </li>
+            <li className="my-px">
+              <a
+                href="#"
+                class="flex flex-row items-center h-10 px-3 rounded-lg text-gray-300 hover:bg-gray-100 hover:text-gray-700"
+              >
+                <Storefront />
+                <span class="ml-3">Products</span>
+              </a>
+            </li>
+            <li className="my-px">
+              <a
+                href="#"
+                class="flex flex-row items-center h-10 px-3 rounded-lg text-gray-300 hover:bg-gray-100 hover:text-gray-700"
+              >
+                <AttachMoney />
+                <span class="ml-3">Transactions</span>
+              </a>
+            </li>
+            <li className="my-px">
+              <a
+                href="#"
+                class="flex flex-row items-center h-10 px-3 rounded-lg text-gray-300 hover:bg-gray-100 hover:text-gray-700"
+              >
+                <BarChart />
+                <span class="ml-3">Reports</span>
+              </a>
+            </li>
+
+            <li class="my-px">
+              <span class="flex font-medium text-sm text-gray-300 px-4 my-4 uppercase">
+                Notifications
+              </span>
+            </li>
+            <li className="my-px">
+              <a
+                href="#"
+                class="flex flex-row items-center h-10 px-3 rounded-lg text-gray-300 hover:bg-gray-100 hover:text-gray-700"
+              >
+                <MailOutline />
+                <span class="ml-3">Mail</span>
+              </a>
+            </li>
+            <li className="my-px">
+              <a
+                href="#"
+                class="flex flex-row items-center h-10 px-3 rounded-lg text-gray-300 hover:bg-gray-100 hover:text-gray-700"
+              >
+                <DynamicFeed />
+                <span class="ml-3">Feedback</span>
+              </a>
+            </li>
+            <li className="my-px">
+              <a
+                href="#"
+                class="flex flex-row items-center h-10 px-3 rounded-lg text-gray-300 hover:bg-gray-100 hover:text-gray-700"
+              >
+                <ChatBubbleOutline />
+                <span class="ml-3">Messages</span>
+              </a>
+            </li>
+            <li class="my-px">
+              <span class="flex font-medium text-sm text-gray-300 px-4 my-4 uppercase">
+                Staff
+              </span>
+            </li>
+            <li className="my-px">
+              <a
+                href="#"
+                class="flex flex-row items-center h-10 px-3 rounded-lg text-gray-300 hover:bg-gray-100 hover:text-gray-700"
+              >
+                <WorkOutline />
+                <span class="ml-3">Manage</span>
+              </a>
+            </li>
+            <li className="my-px">
+              <a
+                href="#"
+                class="flex flex-row items-center h-10 px-3 rounded-lg text-gray-300 hover:bg-gray-100 hover:text-gray-700"
+              >
+                <Timeline />
+                <span class="ml-3">Analytics</span>
+              </a>
+            </li>
+            <li className="my-px">
+              <a
+                href="#"
+                class="flex flex-row items-center h-10 px-3 rounded-lg text-gray-300 hover:bg-gray-100 hover:text-gray-700"
+              >
+                <Report />
+                <span class="ml-3">Reports</span>
+              </a>
             </li>
           </ul>
         </div>
-        <div className="sidebarMenu">
-          <h3 className="sidebarTitle">Quick Menu</h3>
-          <ul className="sidebarList">
-            <Link to="/users" className="link">
-              <li className="sidebarListItem">
-                <PermIdentity className="sidebarIcon" />
-                Users
-              </li>
-            </Link>
-            <Link to="/products" className="link">
-              <li className="sidebarListItem">
-                <Storefront className="sidebarIcon" />
-                Products
-              </li>
-            </Link>
-            <li className="sidebarListItem">
-              <AttachMoney className="sidebarIcon" />
-              Transactions
-            </li>
-            <li className="sidebarListItem">
-              <BarChart className="sidebarIcon" />
-              Reports
-            </li>
-          </ul>
-        </div>
-        <div className="sidebarMenu">
-          <h3 className="sidebarTitle">Notifications</h3>
-          <ul className="sidebarList">
-            <li className="sidebarListItem">
-              <MailOutline className="sidebarIcon" />
-              Mail
-            </li>
-            <li className="sidebarListItem">
-              <DynamicFeed className="sidebarIcon" />
-              Feedback
-            </li>
-            <li className="sidebarListItem">
-              <ChatBubbleOutline className="sidebarIcon" />
-              Messages
-            </li>
-          </ul>
-        </div>
-        <div className="sidebarMenu">
-          <h3 className="sidebarTitle">Staff</h3>
-          <ul className="sidebarList">
-            <li className="sidebarListItem">
-              <WorkOutline className="sidebarIcon" />
-              Manage
-            </li>
-            <li className="sidebarListItem">
-              <Timeline className="sidebarIcon" />
-              Analytics
-            </li>
-            <li className="sidebarListItem">
-              <Report className="sidebarIcon" />
-              Reports
-            </li>
-          </ul>
-        </div>
-      </div>
+      </aside>
     </div>
   );
 }
--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,5 +1,38 @@
 import React from "react";
+import { NotificationsNone, Language, Settings } from "@mui/icons-material";
 
 export default function TopBar() {
-  return <div>TopBar</div>;
+  return (
+    <div className="topbar container h-12 bg-white sticky top-0 z-50">
+      <div className="topbarWrapper h-full py-0 px-5 flex items-center justify-between">
+        <div className="topLeft">
+          <span className="logo font-bold text-3xl cursor-pointer text-blue-900">
+            VCA
+          </span>
+        </div>
+        <div className="topRight flex items-center">
+          <div className="topbarIconContainer relative cursor-pointer mx-3 text-gray-500">
+            <NotificationsNone />
+            <span className="topIconBadge w-4 h-4 absolute -top-1 right-0 bg-red text-white rounded-full flex items-center justify-center text-xs">
+              2
+            </span>
+          </div>
+          <div className="topbarIconContainer relative cursor-pointer mx-3 text-gray-500">
+            <Language />
+            <span className="topIconBadge w-4 h-4 absolute -top-1 right-0 bg-red text-white rounded-full flex items-center justify-center text-xs">
+              2
+            </span>
+          </div>
+          <div className="topbarIconContainer relative cursor-pointer mx-3 text-gray-500">
+            <Settings />
+          </div>
+          <img
+            src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            alt=""
+            className="topAvatar w-10 h-10 rounded-full cursor-pointer"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
